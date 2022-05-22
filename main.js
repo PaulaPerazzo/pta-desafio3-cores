0ba3603fad61f688c83e694df72dd209a40ca05f
PaulaPerazzo <mpperazzosampaio@gmail.com>
2022-05-21 22:56:02 -0300

    hmhmhm

---
+++ main.js
@@ -0,0 +1,8 @@
+const button = document.querySelector(".button");
+
+let changeBody = document.querySelector('body')
+
+button.addEventListener('click', () => {
+    button.style.background = '#ffffff'
+    changeBody.style.background = '#ffffff'
+})
