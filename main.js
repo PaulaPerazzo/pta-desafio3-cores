9669632012ef1e8b5628c29255e18ea4e81140f5
PaulaPerazzo <mpperazzosampaio@gmail.com>
2022-05-23 14:07:09 -0300

    changing colors and texts; requires button fix

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 const button = document.querySelector(".button");
 
-let changeBody = document.querySelector('body');
+let body = document.querySelector('body');
 
 var backgroundName = document.getElementById('#background-name');
 
-button.addEventListener('click', () => {
-    // changeBody.classList.toggle('f2');
-    changeBody.classList.replace('body', 'f2');
-});
+var cont = 0
 
+button.addEventListener('click', () => {
+    body.classList.add('f2')
+    document.getElementById("background-name").textContent = 'Background: #F2F2F2'
+    cont += 1
+
+    if(body.classList.contains('f2') && cont >= 2) {
+        body.classList.replace('f2', 'c7')
+        document.getElementById("background-name").textContent = 'Background: #C7C7C7'
+    };
+
+    if(body.classList.contains('c7') && cont >= 3) {
+        body.classList.remove('c7')
+        document.getElementById("background-name").textContent = 'Background: #292929'
+        cont = 0
+    };
+});
